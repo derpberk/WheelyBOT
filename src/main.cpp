d858fe444504daba69ca5d0559f10caea69fcd0f
Alerodpri <alerodpri@gmail.com>
2023-05-08 16:15:49 +0200

    -Se implementa recepcion de datos mediante trama con formato (Primera version sin crc). -Se implementan las ecuaciones para obtener las velocidades necesarias en cada rueda segun la referencia recibida

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <Arduino.h>
 #include "utils/PID.h"
 
-//vel max 35.7 mc/s
+//vel max ?? mc/s
+//Dif. mode
 
 // Define the target and current values
-float y_ref = 30.0;
+float y_ref[4] = {30,30,30,30};
 float y[4] = {0,0,0,0};
 
 // Define the control signal
@@ -24,9 +25,15 @@ volatile long enc_count[4] = {0,0,0,0};
 
 //Define the variables to track the state of rotation
 volatile char sent[4] = {0,0,0,0};
- 
-String encdir ="";
 
+//Define the variables for data transmission
+const int BUFFER_SIZE = 10;
+const char STX = '\x24';
+const char ETX = '\x3b';
+char buf[BUFFER_SIZE];
+int vel, velA;
+
+ 
 PID PIDs[4]={PID(5, 0.03, 1, 8, 9, 255, 0, sample_time), //mot 1 
              PID(5, 0.03, 1, 6, 7, 255, 0, sample_time), //mot 2
              PID(5, 0.03, 1, 4, 5, 255, 0, sample_time), //mot 3
@@ -53,24 +60,45 @@ void setup() {
   pinMode(28, INPUT_PULLUP); //CLK_enc mot2
   pinMode(29, INPUT_PULLUP); //DT_enc
 
-  //pinMode(30, INPUT_PULLUP); //CLK_enc mot3
-  //pinMode(31, INPUT_PULLUP); //DT_enc
+  pinMode(30, INPUT_PULLUP); //CLK_enc mot3
+  pinMode(31, INPUT_PULLUP); //DT_enc
 
-  //pinMode(32, INPUT_PULLUP); //CLK_enc mot4
-  //pinMode(33, INPUT_PULLUP); //DT_enc
+  pinMode(32, INPUT_PULLUP); //CLK_enc mot4
+  pinMode(33, INPUT_PULLUP); //DT_enc
 
-  attachInterrupt(digitalPinToInterrupt(26),enc_isr1,CHANGE);
-  attachInterrupt(digitalPinToInterrupt(28),enc_isr2,CHANGE);
-  //attachInterrupt(digitalPinToInterrupt(30),enc_isr3,CHANGE);
-  //attachInterrupt(digitalPinToInterrupt(32),enc_isr4,CHANGE);
+  attachInterrupt(digitalPinToInterrupt(26),enc_isr1,CHANGE); //Enc Mot1
+  attachInterrupt(digitalPinToInterrupt(28),enc_isr2,CHANGE); //Enc Mot2
+  attachInterrupt(digitalPinToInterrupt(30),enc_isr3,CHANGE); //Enc Mot3
+  attachInterrupt(digitalPinToInterrupt(32),enc_isr4,CHANGE); //Enc Mot4
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  // check if data is available
+  if (Serial.available() > 0) {
+    if (Serial.read() == STX)
+    {
+      // read the incoming bytes:
+      int rlen = Serial.readBytesUntil(ETX, buf, BUFFER_SIZE);
+
+      vel = (buf[0] -'0') * 10 + buf[1] - '0';
+      velA = (buf[3] -'0') * 10 + buf[4] - '0';
+
+      //Add CRC When jetson gets implemented
+
+    }
+  }
+
+  //Obtain desired speed for each pair of motors
+  y_ref[0]=(2*vel-velA*30)/(2); //velangular=(2*vel-velA*L)/(2*R); vel lineal = velANG*R
+  y_ref[1]=(2*vel+velA*30)/(2); //cambiar 30 por la distancia entre ruedas real
+  y_ref[2]=y_ref[0];
+  y_ref[3]=y_ref[1];
+
   // Get the current speed
   for(int i=0; i<4; i++){
-    if(enc_count[i]>=400 || enc_count[i]<=-400){
+    if(enc_count[i]>=200 || enc_count[i]<=-200){
       enc_count[i]=0;
 
       enc_end[i]=millis();
@@ -85,12 +113,10 @@ void loop() {
   current_time = millis();
   time_change = current_time - prev_time;
 
-  if(millis()>=10000) y_ref=-30;
-
   // Wait for the sampling time
   if (time_change >= sample_time) {
     for(int i=0; i<4; i++){
-      PIDs[i].update(y[i],y_ref,sent[i]);
+      PIDs[i].update(y[i],y_ref[i],sent[i]);
       u[i] = PIDs[i].getU();
     }
 
@@ -134,11 +160,11 @@ void enc_isr3(){
 // the encoder is rotating clockwise
   if (digitalRead(30) != digitalRead(31)) {
     enc_count[2] ++;
-    sent[2]= '1';
+    sent[2]= '0';
    } else {
     // Encoder is rotating counterclockwise
     enc_count[2] --;
-    sent[2]= '0';
+    sent[2]= '1';
    }
 
 }
@@ -159,7 +185,13 @@ void enc_isr4(){
 void speed_data(){
   Serial.print(time_change);
   Serial.print(" ");
-  Serial.print(y_ref);
+  Serial.print(y_ref[0]);
+  Serial.print(" ");
+  Serial.print(y_ref[1]);
+  Serial.print(" ");
+  Serial.print(y_ref[2]);
+  Serial.print(" ");
+  Serial.print(y_ref[3]);
   Serial.print(" ");
   Serial.print(y[0]);
   Serial.print(" ");
@@ -172,6 +204,10 @@ void speed_data(){
   Serial.print(sent[0]);
   Serial.print(" ");
   Serial.print(sent[1]);
+  Serial.print(" ");
+  Serial.print(sent[2]);
+  Serial.print(" ");
+  Serial.print(sent[3]);
   Serial.print(" ");
   Serial.print(u[0]);
   Serial.print(" ");
